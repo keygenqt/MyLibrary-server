98ee8df6d9ea15c4dacc384dda4e2e71cdd71f81
keygenqt <keygenqt@gmail.com>
2020-12-12 02:49:28 +0300

    notifications-deeplink

--- mylibrary/ext/firebase_messaging.py
+++ mylibrary/ext/firebase_messaging.py
@@ -17,8 +17,12 @@ def send_to_token(model):
         android=messaging.AndroidConfig(
             notification=messaging.AndroidNotification(
                 channel_id=model.channel_id,
-                click_action='https://keygenqt.com/licenses'
+                click_action='android.intent.action.DEEP_LINK'
             ),
         ),
+        data={
+            # 'uri': 'http://192.168.1.68:8080/licenses'
+            'uri': 'http://192.168.1.68:8080/books/1'
+        },
         token=model.message_token,
     ))
